3cb049a8dabd01c24a50b51d65eb48eb1a02b6d2
dhaval-jain <61959905+dhaval-jain@users.noreply.github.com>
2020-07-26 14:50:11 +0530

    add customer if new gst_no

--- class_customer.py
+++ class_customer.py
@@ -9,38 +9,33 @@ class customer:
     customer_debit = 0
 
     def customer_detail_input(self):
-        
         print("PLEASE ENTER THE NAME OF THE CUSTOMER")
         self.customer_name = input()
-        
         print("PLEASE ENTER THE CONTACT NUMBER OF THE CUSTOMER")
         self.customer_number = input()
-        
         print("PLEASE ENTER THE GST.NO OF THE CUSTOMER")
         self.gstno = input()
-        
         print("PLEASE ENTER THE ADDRESS OF THE CUSTOMER")
         self.address = input()
         
     def database_customer(self):
-        
         connection_customer = sqlite3.connect("PROJECT.db")
-
         cursor_customer = connection_customer.cursor()
-
         cursor_customer.execute('''CREATE TABLE IF NOT EXISTS customer(
+                        customer_name TEXT,
                         customer_number INT NOT NULL,
                         customer_gst_number INT NOT NULL,
                         customer_address TEXT NOT NULL
                     )'''
         )
-
-        cursor_customer.execute('''INSERT INTO customer(customer_number, customer_gst_number, customer_address) VALUES(?, ?, ?)''',
-                               (self.customer_number,self.gstno,self.address)
-        )
+        cursor_customer.execute('SELECT customer_gst_number FROM customer WHERE customer_gst_number = {} '.format(self.gstno))
+        if cursor_customer.fetchone():
+            print("Customer with gstno {} already exists".format(self.gstno))
+        else:
+            cursor_customer.execute('''INSERT INTO customer(customer_name, customer_number, customer_gst_number, customer_address) VALUES(?, ?, ?, ?)''',
+                               (self.customer_name, self.customer_number,self.gstno,self.address))
 
         connection_customer.commit()
-
         connection_customer.close()
 
 
