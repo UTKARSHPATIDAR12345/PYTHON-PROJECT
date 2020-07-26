2b6865fbc306ad885f1896e3f40acc960186a89a
aadarshjain <62375555+aadarshjain@users.noreply.github.com>
2020-07-26 14:25:17 +0530

    Update class_customer.py

--- class_customer.py
+++ class_customer.py
@@ -1,36 +1,42 @@
 import sqlite3
 
 class customer:
-    customer_name=" "
-    contact_number=" "
-    gstno=" "
-    address=" "
-    customer_debit=0
+    
+    customer_name = " "
+    contact_number = " "
+    gstno = " "
+    address = " "
+    customer_debit = 0
 
     def customer_detail_input(self):
+        
         print("PLEASE ENTER THE NAME OF THE CUSTOMER")
-        self.customer_name=input()
+        self.customer_name = input()
+        
         print("PLEASE ENTER THE CONTACT NUMBER OF THE CUSTOMER")
-        self.customer_number=input()
+        self.customer_number = input()
+        
         print("PLEASE ENTER THE GST.NO OF THE CUSTOMER")
-        self.gstno=input()
+        self.gstno = input()
+        
         print("PLEASE ENTER THE ADDRESS OF THE CUSTOMER")
-        self.address=input()
+        self.address = input()
         
     def database_customer(self):
-        connection_customer=sqlite3.connect("PROJECT.db")
+        
+        connection_customer = sqlite3.connect("PROJECT.db")
 
-        cursor_customer=connection_customer.cursor()
+        cursor_customer = connection_customer.cursor()
 
         cursor_customer.execute('''CREATE TABLE IF NOT EXISTS customer(
-            customer_number TEXT NOT NULL,
-            customer_gst_number TEXT NOT NULL,
-            customer_address TEXT NOT NULL
-        )'''
+                        customer_number INT NOT NULL,
+                        customer_gst_number INT NOT NULL,
+                        customer_address TEXT NOT NULL
+                    )'''
         )
 
-        cursor_customer.execute('''INSERT INTO customer(customer_number,customer_gst_number,customer_address) VALUES(?,?,?)''',
-        (self.customer_number,self.gstno,self.address)
+        cursor_customer.execute('''INSERT INTO customer(customer_number, customer_gst_number, customer_address) VALUES(?, ?, ?)''',
+                               (self.customer_number,self.gstno,self.address)
         )
 
         connection_customer.commit()
@@ -38,6 +44,6 @@ class customer:
         connection_customer.close()
 
 
-customerobj=customer()
+customerobj = customer()
 customerobj.customer_detail_input()
 customerobj.database_customer()
