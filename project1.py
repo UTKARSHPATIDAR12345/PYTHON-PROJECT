fe67abe6bd93937d65b39020d9a2bdb5c51acfed
aadarshjain <62375555+aadarshjain@users.noreply.github.com>
2020-07-27 23:53:23 +0530

    changed chronology

--- project1.py
+++ project1.py
@@ -4,8 +4,14 @@ from class_customer import *
 from class_purchase import *
 from class_sale import *
 
-#calling class_sale function
+#calling class_customer function
+print("##########CUSTOMERS DETAILS###########")
+cus = customer()
+cus.customer_detail_input()
+cus.database_customer()
 
+#calling class_sale function
+print("##########SALE DETAILS###########")
 sell = sales()
 sell.input_customer()
 sell.input_company()
@@ -14,15 +20,10 @@ sell.products()
 sell.update_database()
 
 #calling class_purchase function
-
+print("##########PURCHASE DETAILS###########")
 pur = purchase()
 pur.input_company()
 pur.input_product()
 pur.products()
 pur.update_purchase_database()
 
-#calling class_customer function
-
-cus = customer()
-cus.customer_detail_input()
-cus.database_customer()
